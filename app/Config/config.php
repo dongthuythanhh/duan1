9c2a641ca1b521fe19f3f6e2758fb923b4e5c40b
dongthuythanhh <thanhthanhdong2003@gmail.com>
2023-10-03 01:03:35 +0700

    create DA1

---
+++ app/Config/config.php
@@ -0,0 +1,7 @@
+<?php
+const DSN = "mysql:host=localhost;dbname=project;charset=utf8";
+const USER = "root";
+const PASSWORD = "";
+const URL = "http://localhost/DuAn1-FPT/";
+define('APP_ROOT',dirname(__DIR__));
+?>
